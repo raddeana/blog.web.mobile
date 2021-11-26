56e92453738704c4c7e8f2a931dd16b570c510cf
Chen Xiangyu <wowcxy2008@126.com>
2021-11-26 16:23:40 +0800

    feat: sth

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,7 @@
 <template>
-    <van-form @submit="onSubmit">
+<div class="login-page">
+    <h4 class="title">登录</h4>
+    <van-form class="login-form" @submit="onSubmit">
         <van-field
             v-model="username"
             name="用户名"
@@ -19,8 +21,44 @@
             <van-button round block type="info" native-type="submit">提交</van-button>
         </div>
     </van-form>
+</div>
 </template>
+<style scoped lang="less">
+.login-page {
+    overflow: hidden;
+    height: 100vh;
+    width: 100vw;
+    background: linear-gradient(-90deg, #4caf50 1px, transparent 1px) 0% 0% / 20px 20px, linear-gradient(#4caf50 1px, transparent 1px) 0% 0% / 20px 20px;
+}
 
+.title {
+    font-size: 26px;
+    margin: 15vh 0 5vh 0;
+    text-align: center;
+    color: #4caf50;
+    letter-spacing: 26px;
+    text-indent: 26px;
+}
+
+.login-form {
+    margin: 4vh 5vw;
+    padding: 4vh 5vw;
+    background-color: rgb(76 175 80 / 60%);
+    border-radius: 10px;
+}
+
+.van-cell {
+    background-color: transparent;
+}
+
+/deep/ .van-field__label{
+    color: #ffeb3b;
+}
+
+/deep/ .van-field__control {
+    color: #fff;
+}
+</style>
 <script lang="ts">
 import Vue from 'vue'
 
